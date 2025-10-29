4951977a94d794058607da9ba21eefa9a4742ebd
Graeme Byrne <grmbrn89@gmail.com>
2025-10-30 00:13:10 +0100

    Remove target directory from Git

--- target/debug/deps/mini_torrust-a02a96d8b304fab4.d
+++
@@ -1,6 +0,0 @@
-/Users/macbookair/Desktop/mini-torrust/target/debug/deps/libmini_torrust-a02a96d8b304fab4.rmeta: src/main.rs src/models.rs
-
-/Users/macbookair/Desktop/mini-torrust/target/debug/deps/mini_torrust-a02a96d8b304fab4.d: src/main.rs src/models.rs
-
-src/main.rs:
-src/models.rs:
--- target/debug/deps/mini_torrust-f4a831f37a2d9d8a.d
+++
@@ -1,6 +0,0 @@
-/Users/macbookair/Desktop/mini-torrust/target/debug/deps/libmini_torrust-f4a831f37a2d9d8a.rmeta: src/main.rs src/models.rs
-
-/Users/macbookair/Desktop/mini-torrust/target/debug/deps/mini_torrust-f4a831f37a2d9d8a.d: src/main.rs src/models.rs
-
-src/main.rs:
-src/models.rs:
